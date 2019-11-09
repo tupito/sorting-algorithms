7a13965b22b94aa090ec2198e11edcb0ba90b563
TT <tomi.tupiini@gmail.com>
2019-11-09 14:27:16 +0200

    some kind of deletage version

--- Program.cs
+++ Program.cs
@@ -1,52 +1,140 @@
 ﻿using System;
+using System.Diagnostics; // stopwatch
 
 namespace Kotitehtava_Delegate
 {
     class Program
     {
         public delegate void SortDelegate(int[] num);
-        public delegate int[] CreateArrayDelegate(int size);
-
+        //public delegate int[] CreateArrayDelegate(int size);
 
         static int[] CreateRandomArray(int size)
         {
-            // tee taulukko satunnaislukuja
-            return new int[size];
+            int[] num = new int[size];
+            Random generator = new Random();
+            for (int i = 0; i < num.Length; i++)
+            {
+                num[i] = generator.Next(size);
+            }
+            return num;
         }
 
-        static void Sort1(int[] num)
+        static void SelectionSort(int[] num)
         {
-            Console.WriteLine("Sort 1");
+            Console.WriteLine("\nSelectionSort: ");
+            int i, j, first, temp;
+            for (i = num.Length - 1; i > 0; i--)
+            {
+                first = 0;   //initialize to subscript of first element
+                for (j = 1; j <= i; j++)   //locate smallest element between positions 1 and i.
+                {
+                    if (num[j] > num[first])
+                        first = j;
+                }
+                temp = num[first];   //swap smallest found with element in position i.
+                num[first] = num[i];
+                num[i] = temp;
+            }
         }
 
-        static void Sort2(int[] num)
+        static void InsertionSort(int[] num)
         {
-            Console.WriteLine("Sort 2");
+            Console.WriteLine("\nInsertionSort");
+
+            for (int i = 1; i < num.Length; i++)
+            {
+                int key = num[i];
+                int j = i - 1;
+                while (j >= 0 && num[j] > key)
+                {
+                    //loop
+                    num[j + 1] = num[j];
+                    j--;
+                }
+                num[j + 1] = key;
+            }
+        }
+
+        private static int Partition(int[] num, int lo, int hi)
+        {
+            int pivot = num[lo];
+            while (true)
+            {
+
+                while (num[lo] < pivot)
+                {
+                    lo++;
+                }
+
+                while (num[hi] > pivot)
+                {
+                    hi--;
+                }
+
+                if (lo < hi)
+                {
+                    if (num[lo] == num[hi]) return hi;
+
+                    int temp = num[lo];
+                    num[lo] = num[hi];
+                    num[hi] = temp;
+                }
+                else
+                {
+                    return hi;
+                }
+            }
         }
 
         static void QuickSort(int[] num, int lo, int hi)
         {
+            if (lo < hi)
+            {
+                int pivot = Partition(num, lo, hi);
 
+                if (pivot > 1)
+                {
+                    QuickSort(num, lo, pivot - 1);
+                }
+                if (pivot + 1 < hi)
+                {
+                    QuickSort(num, pivot + 1, hi);
+                }
+            }
         }
+
+
         static void QuickSortMain(int[] num)
         {
+            Console.WriteLine("\nQuicksort");
             QuickSort(num, 0, num.Length - 1);
         }
 
-        static void Metodi(SortDelegate del)
+        static void Metodi(SortDelegate sortDelegate)
         {
             // luo taulukko (satunnaisluvuilla)
             int[] num = new int[10000];
+            num = CreateRandomArray(10000);
             // ota aika
-            del(num);
-            // laske aikaero
-            // tulosta
+            // käynnistä ajastin
+            Stopwatch kello = Stopwatch.StartNew();
+            sortDelegate(num);
+            // ota aika ja tulosta se
+            var elapsedTime = kello.Elapsed;
+            Console.WriteLine("aika: {0}", elapsedTime);
+            
+            /*foreach (var item in num)
+            {
+                Console.Write($"{item} ");
+            }*/
         }
 
         static void Main(string[] args)
         {
-            SortDelegate ds1 = new SortDelegate(Sort1);
-            SortDelegate ds2 = new SortDelegate(Sort2);
+            Console.WriteLine("Deletage-version");
+
+            SortDelegate ds1 = new SortDelegate(SelectionSort);
+            SortDelegate ds2 = new SortDelegate(InsertionSort);
             SortDelegate quick = new SortDelegate(QuickSortMain);
 
             // testataan sort1
